95bee979caf394d23fd658e461839b3f9d285470
gilbox <birmangil@gmail.com>
2015-06-24 01:35:09 -0700

    fix babel.conf

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
-require('../babel.setup');
+// no babel-specific features allowed in this file,
+// only in files you require below
+
+require('../babel.conf');
 var foo = require('./app').foo;
 
 console.log("foo(55) ===", foo(55));
